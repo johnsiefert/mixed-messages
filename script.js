78e867d1ec1dde7ab0d3bd2975f3266a56391b6d
John <johnsief12@gmail.com>
2023-12-21 18:44:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let questions =
+[
+    {
+        quote: "May the Force be with you",
+        choices: ["Star Wars", "Star Trek", "Rick and Morty"],
+        answer: 0
+    },
+    {
+        quote: "I'm the king of the world!",
+        choices: ["Avator", "Titanic", "King Kong"],
+        answer: 1
+    },
+    {
+        quote: "Elementary, my dear Watson",
+        choices: ["Sherlock Holmes", "Batman", "Castaway"],
+        answer: 0
+    },
+    {
+        quote: "I'll be back",
+        choices: ["Predator", "Jingle All the Way", "Terminator"],
+        answer: 2
+    },
+    {
+        quote: "You're gonna need a bigger boat",
+        choices: ["Titanic", "Jaws", "Hunt for Red October"],
+        answer: 0
+    },
+]
+
+for(let i = 0; i < questions.length; i++){
+    
+}
+console.log(questions)

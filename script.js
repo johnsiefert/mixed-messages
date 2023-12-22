4dc10e7d3d8522b9ab890337fac4cba08d4dce23
John <johnsief12@gmail.com>
2023-12-22 18:04:52 -0500

    changed object to random quotes

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-let questions =
-[
+let quotes = [
     {
-        quote: "May the Force be with you",
-        choices: ["Star Wars", "Star Trek", "Rick and Morty"],
-        answer: 0
+        id: 0,
+        author: "Nelson Mandela",
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall."
     },
     {
-        quote: "I'm the king of the world!",
-        choices: ["Avator", "Titanic", "King Kong"],
-        answer: 1
-    },
-    {
-        quote: "Elementary, my dear Watson",
-        choices: ["Sherlock Holmes", "Batman", "Castaway"],
-        answer: 0
-    },
-    {
-        quote: "I'll be back",
-        choices: ["Predator", "Jingle All the Way", "Terminator"],
-        answer: 2
-    },
-    {
-        quote: "You're gonna need a bigger boat",
-        choices: ["Titanic", "Jaws", "Hunt for Red October"],
-        answer: 0
-    },
-]
+        id: 1,
+        author: "Walt Disney",
+        quote: "The way to get started is to quit talking and begin doing."
+      },
+      {
+        id: 2,
+        author: "Steve Jobs",
+        quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking."
+      },
+      {
+        id: 3,
+        author: "Eleanor Roosevelt",
+        quote: "If life were predictable it would cease to be life, and be without flavor."
+      },
+      {
+        id: 4,
+        author: "Oprah Winfrey",
+        quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."
+      },
+      {
+        id: 5,
+        author: "Oprah Winfrey",
+        quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."
+      },
+      {
+        id: 6,
+        author: "James Cameron",
+        quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."
+      },
+      {
+        id: 7,
+        author: "John Lennon",
+        quote: "Life is what happens when you're busy making other plans."
+      },
+  ];
 
-for(let i = 0; i < questions.length; i++){
-    
+  function randomQuotes (){
+for(let i = 0; i < quotes.length; i++){
+    let phrase = quotes[i].id
+    // let person = quotes[i].author
+    console.log(phrase)
+    // console.log(person)
 }
-console.log(questions)
+  }

bee3afce3e287b53ba59b6d4805d324e364453dd
John <johnsief12@gmail.com>
2023-12-28 10:04:11 -0500

    have the function randomly outputting quotes with the author.

--- script.js
+++ script.js
@@ -1,59 +1,46 @@
 let quotes = [
     {
-        id: 0,
         author: "Nelson Mandela",
         quote: "The greatest glory in living lies not in never falling, but in rising every time we fall."
     },
     {
-        id: 1,
         author: "Walt Disney",
         quote: "The way to get started is to quit talking and begin doing."
       },
       {
-        id: 2,
         author: "Steve Jobs",
         quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking."
       },
       {
-        id: 3,
         author: "Eleanor Roosevelt",
         quote: "If life were predictable it would cease to be life, and be without flavor."
       },
       {
-        id: 4,
         author: "Oprah Winfrey",
         quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."
       },
       {
-        id: 5,
         author: "Oprah Winfrey",
         quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."
       },
       {
-        id: 6,
         author: "James Cameron",
         quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."
       },
       {
-        id: 7,
         author: "John Lennon",
         quote: "Life is what happens when you're busy making other plans."
       },
   ];
 
-  function randomInt() {
-    return Math.floor(Math.random() * 8)
-  }
+const keys = Object.keys(quotes);
+const randomIndex = keys[Math.floor(Math.random() * keys.length)]
+const phrase = quotes[randomIndex]
 
 
   function randomQuotes (){
-    let numId = randomInt()
-for(let i = 0; i < quotes.length; i++){
-    let phrase = quotes[i]
-    let randomPhrase = {};
-    switch(numId){
-        
-    }
-}
-  }
+    const keys = Object.keys(quotes);
+    const randomIndex = keys[Math.floor(Math.random() * keys.length)]
+    const phrase = quotes[randomIndex]
+console.log(phrase)}
   randomQuotes ()
